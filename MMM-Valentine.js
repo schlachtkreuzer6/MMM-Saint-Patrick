dda740a0c194cb267ce002c25245e0713008e4bd
schlachtkreuzer6 <stefan.kreuzer2014@gmail.com>
2017-02-04 13:20:43 +0100

    MMM-Saint-Patrick.js

--- MMM-Valentine.js
+++ MMM-Valentine.js
@@ -1,48 +1,48 @@
 /* Magic Mirror
- * Module: MMM-Valentine
+ * Module: MMM-Saint-Patrick
  *
- * By Sergey Morozov
+ * By Sergey Morozov and forked by schlachtkreuzer6
  * Based on https://github.com/MichMich/MMM-Snow
  * MIT Licensed.
  */
 
-Module.register("MMM-Valentine",{
+Module.register("MMM-Saint-Patrick",{
 
 	defaults: {
-		valentinesCount: 50,
-		valentinesSize: 1.00
+		patricksCount: 50,
+		patricksSize: 1.00
 	},
 
 	getStyles: function() {
-		return [ "MMM-Valentine.css" ]
+		return [ "MMM-Saint-Patrick.css" ]
 	},
 
 	getDom: function() {
 		var wrapper = document.createElement("div")
-		wrapper.className = "valentine-wrapper"
+		wrapper.className = "patrick-wrapper"
 
-		var heart, jiggle, size, rotation;
+		var patrick, jiggle, size, rotation;
 
-		for(var i = 0; i < this.config.valentinesCount; i++) {
-			heart = document.createElement("div")
-			heart.className = "valentine-heart"
+		for(var i = 0; i < this.config.patricksCount; i++) {
+			patrick = document.createElement("div")
+			patrick.className = "saint-patrick"
 
 			jiggle = document.createElement("div");
 			jiggle.style.animationDelay = (Math.random() * 4) + "s";
 			jiggle.style.animationDuration = ((Math.random() * 30) + 30) + "s";
-			jiggle.className = "heart" + (Math.round(Math.random() * 14) + 1);
+			jiggle.className = "patrick" + (Math.round(Math.random() * 14) + 1);
 
-			size = (Math.random() * 0.75) + 0.25 + this.config.valentinesSize;
+			size = (Math.random() * 0.75) + 0.25 + this.config.patricksSize;
 			rotation = (Math.floor(Math.random() * 81) - 40);
 			jiggle.style.transform = "scale(" + size + ", " + size + ") rotate(" + rotation + "deg)";
-			jiggle.style.opacity = size - this.config.valentinesSize;
+			jiggle.style.opacity = size - this.config.patricksSize;
 
-			heart.appendChild(jiggle);
-			heart.style.left = ((Math.random() * 100) - 10) + "%";
-			heart.style.animationDelay = (Math.random() * 100) + "s";
-			heart.style.animationDuration = 100 - (Math.random() * 50 * (size - this.config.valentinesSize)) + "s";
+			patrick.appendChild(jiggle);
+			patrick.style.left = ((Math.random() * 100) - 10) + "%";
+			patrick.style.animationDelay = (Math.random() * 100) + "s";
+			patrick.style.animationDuration = 100 - (Math.random() * 50 * (size - this.config.patricksSize)) + "s";
 
-			wrapper.appendChild(heart);
+			wrapper.appendChild(patrick);
 		}
 		Log.log(wrapper);
 		return wrapper;
